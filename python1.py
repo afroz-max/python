cfbddc0960a9a7e089d6433ed596f512b47b93bc
Afroz <your-real-github-email@example.com>
2026-09-17 23:00:09 +0530

    Update python1.py

--- python1.py
+++ python1.py
@@ -147,3 +147,64 @@ import datetime
 print(datetime.datetime.now())
 
 help("modules")
+
+#loops in python
+#1.while loop
+
+number = int(input("enter the number"))
+i = 1
+while i<11:
+    print(number *i)
+    i = i + 1
+
+#while loop with else
+x = 1
+while x<3:
+    print(x)
+    x += 1
+else:
+    print("limit crossed")
+
+#guessing game
+#gernerate a random integer between 1 to 100
+import random
+
+jackpot = random.randint(1, 100)
+
+guess = int(input("Guess the number: "))
+
+while guess != jackpot:
+    if guess < jackpot:
+        print("Wrong! Guess higher.")
+    else:
+        print("Wrong! Guess lower.")
+
+    guess = int(input("Guess again: "))
+    counter += 1
+else :
+    print("correct guess")
+    print("attempts",counter)
+
+#for loop demo
+for i in range(1,11):
+    print(i)
+
+for i in range(1,11):
+    print(i)
+
+for i in range(1,11,4):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
+
+for i in "delie":
+    print(i)
+
+#for loop
+curr_pop = 1000
+for i in range(10, 0, -1):
+    print(i,curr_pop)
+    curr_pop = curr_pop - 0.1 * curr_pop
+
+ 
